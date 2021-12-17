c7e640f66bab20c276023be97027ae27fb94585d
Oh Donggeon <donggeon.dev@gmail.com>
2021-12-17 20:37:23 +0900

    Initial Commit

---
+++ ModuTyping/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ModuTyping
+//
+//  Created by Oh Donggeon on 2021/12/17.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
